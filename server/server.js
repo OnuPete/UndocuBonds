52896312fcbf03578e84317b7e07d45bcce46cd5
Jerry Jong <jerryjong01@gmail.com>
2017-09-09 14:50:32 -0700

    initial commit react skeleton + simple express server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname +'./../')); //serves the index.html
+app.use(express.static(__dirname +'./../bundle')); //serves the build
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log('App listening on port ' + port)
+});
