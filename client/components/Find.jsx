8aee90978a3040e0a93237dd279781a79a77f139
OnuPete <pcr137@case.edu>
2017-09-09 21:05:06 -0700

    added onclick to states and community funds page

--- client/components/Find.jsx
+++ client/components/Find.jsx
@@ -2,60 +2,65 @@ import React, { Component } from 'react';
 import { List, ListItem } from 'material-ui/List';
 import Paper from 'material-ui/Paper'
 
-let states = [
-'Alabama', 
-'Alaska',  
-'Arizona', 
-'Arkansas', 
-'California', 
-'Colorado', 
-'Connecticut', 
-'Delaware', 
-'Florida', 
-'Georgia', 
-'Hawaii', 
+const states = [
+'Alabama',
+'Alaska',
+'Arizona',
+'Arkansas',
+'California',
+'Colorado',
+'Connecticut',
+'Delaware',
+'Florida',
+'Georgia',
+'Hawaii',
 'Idaho',
 'Illinois',
 'Indiana',
 'Iowa',
-'Kansas', 
-'Kentucky', 
-'Louisiana', 
-'Maine', 
-'Maryland', 
-'Massachusetts', 
-'Michigan', 
-'Minnesota', 
-'Mississippi', 
-'Missouri', 
-'Montana', 
-'Nebraska', 
-'Nevada', 
-'New Hampshire', 
-'New Jersey', 
-'New Mexico', 
-'New York', 
+'Kansas',
+'Kentucky',
+'Louisiana',
+'Maine',
+'Maryland',
+'Massachusetts',
+'Michigan',
+'Minnesota',
+'Mississippi',
+'Missouri',
+'Montana',
+'Nebraska',
+'Nevada',
+'New Hampshire',
+'New Jersey',
+'New Mexico',
+'New York',
 'North Carolina',
 'North Dakota',
-'Ohio', 
-'Oklahoma', 
-'Oregon', 
-'Pennsylvania', 
+'Ohio',
+'Oklahoma',
+'Oregon',
+'Pennsylvania',
 'Rhode Island',
 'South Carolina',
-'South Dakota', 
-'Tennessee', 
-'Texas', 
-'Utah', 
-'Vermont', 
-'Virginia', 
-'Washington', 
-'West Virginia', 
-'Wisconsin', 
+'South Dakota',
+'Tennessee',
+'Texas',
+'Utah',
+'Vermont',
+'Virginia',
+'Washington',
+'West Virginia',
+'Wisconsin',
 'Wyoming'
-]
+];
 
-class Find extends Component { 
+const description = {
+  'California': 'We da Best',
+  'New York': 'cry cry all the time'
+}
+
+class Find extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
@@ -83,9 +88,20 @@ class Find extends Component {
         <List style={{width: '15%', height: '840px', padding: 0, overflow: 'scroll'}}>
           {this.mapStates()}
         </List>
-        <Paper zDepth={0} style={{width: '85%', paddingTop: '10px'}}>Lorem Ipsom cudle da too</Paper>
+        <Paper zDepth={0} style={styles.paperStyle}>
+          <p>Lorem Ipsom cudle da too</p>
+          <p>{description[this.state.currentState]}</p>
+        </Paper>
       </div>
     )
+  }
+}
+
+const styles = {
+  paperStyle: {
+    textAlign: 'center',
+    width: '85%',
+    paddingTop: '10px'
   }
 }
 
